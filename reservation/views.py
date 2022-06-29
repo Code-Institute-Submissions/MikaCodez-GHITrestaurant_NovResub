61dff55b8cc7159998e42825e8d692e864ca3307
Malcolm MikaZuki <malcolm.wiredu@gmail.com>
2022-06-29 18:31:54 +0000

    fixed crud issues can now do full crud

--- reservation/views.py
+++ reservation/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse, redirect
 from django.views import generic, View
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.base import TemplateView
