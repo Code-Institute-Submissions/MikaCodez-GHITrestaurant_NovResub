dd9d29f47fe1200e8277991ca9d1e61aaf7d26fb
Malcolm MikaZuki <malcolm.wiredu@gmail.com>
2022-06-29 21:12:43 +0000

    cleaning up sign and registration forms

--- reservation/urls.py
+++ reservation/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path("", views.ReservationList.as_view(), name="reservation"),
     path('reservation_detail/', views.ReservationList.as_view(), name='reservation_detail'),
     path("publish_reservation", views.CreateReservation.as_view(), name="publish_reservation"),
-    path("<pk>/update", views.UpdateReservation.as_view(), name="update_reservation"),
-    path("<pk>/delete", views.DeleteReservation.as_view(), name="delete_reservation"),
+    path('update_reservation/', views.UpdateReservation.as_view(), name="update_reservation"),
+    path('delete_reservation/', views.DeleteReservation.as_view(), name="delete_reservation"),
 ]
